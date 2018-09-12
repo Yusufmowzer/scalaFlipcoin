09cf6df107ba0ee440a89fdd4b2a28fcd45c2f02
Yusuf <yusuf@fullfacing.com>
2018-09-12 10:45:05 +0200

    Refactored styling. Added break line between new screens. Implemented error handling for menu screen. Implemented menu option for game 2

--- src/main/scala/CoinFlip.scala
+++ src/main/scala/CoinFlip.scala
@@ -1,20 +1,29 @@
 import CoinFlipUtils._
-import scala.annotation.tailrec
+
 import scala.util.Random
 
 case class GameState(numFlips: Int, numCorrect: Int)
 
 object CoinFlip extends App {
+  print(Console.BLUE_B)
+  print("_______________________________")
+  print(Console.RESET)
+  println()
+  print(Console.RED)
+  print(Console.BOLD)
+  print("         - Welcome -           ")
+  print(Console.RESET)
+  println()
+  homeScreen()
+  println()
+  print(Console.RED_B)
+  print("_______________________________")
+  print(Console.RESET)
+  selectScreen()
 
-  val r = Random
-  val s = GameState(0, 0)
-  mainLoop(s, r)
+  def mainLoop(gameState: GameState, random: Random, gameSelect: Int) {
 
-  @tailrec
-  def mainLoop(gameState: GameState, random: Random) {
-
-    showPrompt()
-    val userInput = getUserInput()
+    val userInput = BestOf10(gameSelect, gameState)
 
     // handle the result
     userInput match {
@@ -25,26 +34,56 @@ object CoinFlip extends App {
           val newNumCorrect = gameState.numCorrect + 1
           val newGameState = gameState.copy(numFlips = newNumFlips, numCorrect = newNumCorrect)
           printGameState(printableFlipResult(coinTossResult), newGameState, "Awesome")
-          mainLoop(newGameState, random)
+          mainLoop(newGameState, random, gameSelect)
         } else {
           val newGameState = gameState.copy(numFlips = newNumFlips)
           printGameState(printableFlipResult(coinTossResult), newGameState, "You Suck")
-          mainLoop(newGameState, random)
+          mainLoop(newGameState, random, gameSelect)
         }
       }
       case "Q" => {
-        printGameOver()
-        printGameState(gameState)
-        winPercentage(gameState)
-        // return out of the recursion here
+        if (gameSelect == 1 && gameState.numFlips == 10) println("\n\nGoodBye!")
+        else {
+          printGameOver()
+          printGameState(gameState)
+          winPercentage(gameState)
+          println("\nGoodBye!")
+          // return out of the recursion here
+        }
       }
+
+      case "M" => {
+        if (gameSelect == 1 && gameState.numFlips == 10) {
+          println("\n_______________________________")
+          println("\n")
+          homeScreen()
+          selectScreen()
+        }
+        else {
+          printGameOver()
+          printGameState(gameState)
+          winPercentage(gameState)
+          println("_______________________________")
+          println("\n")
+          homeScreen()
+          selectScreen()
+        }
+      }
+
       case _ ⇒ {
         val newNumFlips = gameState.numFlips
         val newNumCorrect = gameState.numCorrect
-        println("Incorrect input. Please try again")
+        println()
+        print(Console.BOLD)
+        print(Console.RED)
+        print(Console.REVERSED)
+        print("Incorrect input. Please try again")
+        print(Console.RESET)
+        println()
         printGameState(gameState)
+        println("_______________________________")
         val newGameState = gameState.copy(numFlips = newNumFlips, numCorrect = newNumCorrect)
-        mainLoop(newGameState, random)
+        mainLoop(newGameState, random, gameSelect)
       }
     }
   }
--- src/main/scala/CoinFlipUtils.scala
+++ src/main/scala/CoinFlipUtils.scala
@@ -1,11 +1,103 @@
-import scala.util.Random
+import CoinFlip.mainLoop
+
 import scala.io.StdIn.readLine
+import scala.util.Random
 
 object CoinFlipUtils {
+  def homeScreen(): Unit = {
+    print(Console.BLUE)
+    print(Console.BOLD)
+    //    print(Console.REVERSED)
+    print("ooooooooO Flip Coin Ooooooooooo")
+    print(Console.RESET)
+  }
 
-  def showPrompt(): Unit = { print("\n(h)eads, (t)ails, or (q)uit: ") }
+  def selectScreen(): Unit = {
+    println("\n")
+    println("1. Best out of 10")
+    println("2. Free play")
+    println("\n Press q to EXIT")
+    print(Console.BOLD)
+    print(Console.GREEN)
+    print("Choose a game setting (1) or (2): ")
+    print(Console.RESET)
+    val r = Random
+    val s = GameState(0, 0)
+    val userSelect = getUserInput()
+
+    print(Console.BOLD)
+    print(Console.GREEN)
+    println()
+    userSelect match {
+      case "1" ⇒ {
+        println("\nO  --- Best Out of 10 ---  O")
+        mainLoop(s, r, userSelect.toInt)
+      }
+      case "2" ⇒ {
+        println("\nO     --- Free Play ---     O")
+        mainLoop(s, r, userSelect.toInt)
+      }
+      case "Q" ⇒ {
+        println("\n\nGoodBye!")
+        print(Console.RESET)
+      }
+      case _ ⇒ {
+        println()
+        print(Console.BOLD)
+        print(Console.RED)
+        print(Console.REVERSED)
+        print("Incorrect input. Please try again")
+        print(Console.RESET)
+        println("\n\n")
+        homeScreen()
+        selectScreen()
+      }
+    }
+    print(Console.RESET)
+  }
+
+  def BestOf10(gameSelect: Int, gameState: GameState): String = (gameSelect, gameState.numFlips) match {
+    case (1, 10) ⇒ {
+      printGameOver()
+      printGameState(gameState)
+      winPercentage(gameState)
+      print("\nBack to (m)enu or (q)uit:")
+      getUserInput()
+    }
+    case _ ⇒ {
+      showPrompt()
+      getUserInput()
+    }
+  }
 
   def getUserInput(): String = readLine.trim.toUpperCase
+
+  def printGameOver(): Unit = {
+    print(Console.RED)
+    print(Console.BOLD)
+    println("\n\n     === GAME OVER === \n")
+    print(Console.RESET)
+  }
+
+  def showPrompt(): Unit = {
+    print(Console.BOLD)
+    print("\n(h)eads, (t)ails, or (m)enu: ")
+    print(Console.RESET)
+  }
+
+  def winPercentage(gameState: GameState): Unit = {
+    print(Console.BOLD)
+    print(Console.BLUE)
+    if (gameState.numFlips != 0) println(f"Win Percentage: ${(gameState.numCorrect.toFloat / gameState.numFlips.toFloat) * 100.00}%1.2f" + "%")
+    else println("Win Percentage: 0.00%")
+    print(Console.RESET)
+  }
+
+  def printGameState(gameState: GameState): Unit = {
+    print(Console.BLUE)
+    println(s"Flips: ${gameState.numFlips}   Correct: ${gameState.numCorrect}")
+    print(Console.RESET)
+  }
 
   def printableFlipResult(flip: String): String = flip match {
     case "H" => "Heads"
@@ -13,18 +105,12 @@ object CoinFlipUtils {
   }
 
   def printGameState(printableFlipResult: String, gameState: GameState, result: String): Unit = {
-    print(s"$result: Flip was $printableFlipResult. \n")
+    print(Console.BOLD)
+    print(Console.RED)
+    print(s"\n$result: Flip was $printableFlipResult. \n")
+    print(Console.RESET)
     printGameState(gameState)
-  }
-
-  def printGameState(gameState: GameState): Unit = {
-    println(s"#Flips: ${gameState.numFlips}, #Correct: ${gameState.numCorrect}")
-  }
-
-  def printGameOver(): Unit = println("\n=== GAME OVER === ")
-
-  def winPercentage(gameState: GameState): Unit = {
-    println(f" Win Percentage: ${(gameState.numCorrect.toFloat/gameState.numFlips.toFloat)*100.00}%1.2f"+"%")
+    println("_______________________________")
   }
 
   // returns "H" for heads, "T" for tails
